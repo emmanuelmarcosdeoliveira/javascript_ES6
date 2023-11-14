c885f7255d851b772fed59a244099b48c044d690
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2023-11-14 18:07:06 -0300

    first commit

---
+++ variables/0201_variaveis/exercise/script.js
@@ -0,0 +1,15 @@
+// Declarar uma variável com o seu nome
+const NOME = "Emmanuel";
+
+// Declarar uma variável com a sua idade
+const IDADE = 44;
+// Declarar uma variável com a sua comida favorita e não atribuir valor
+let comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = "Arroz, feijão, bife e batata Frita";
+// Declarar 5 variáveis diferentes sem valores
+let casa, familia, sexo, carro, corPreferida;
+
+console.log(NOME, IDADE, comidaFavorita);
+console.log(casa, familia, sexo, carro, corPreferida);
