1d9f9000e49782d58694e53f83a82afffe1f73b8
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2023-11-21 23:03:28 -0300

    classroom_0203

---
+++ classes/modules_02/0203_Numeros_e_Operadores-pt2/scripts.js
@@ -0,0 +1,13 @@
+var soma1 = 10 + 10 + 20 + (30 * 4) / 2 + 10;
+console.log(soma1);
+
+var x = 5;
+console.log(x++);
+console.log(x);
+
+var idade = "28";
+console.log(idade);
+
+var somaIdade = 5;
+
+console.log(-idade + somaIdade);
